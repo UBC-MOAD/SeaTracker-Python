79e034c0f4c896b27f5884b82c1b88587eba3e96
Doug Latornell <djl@douglatornell.ca>
2018-01-07 10:51:34 -0500

    Add NEMO model results field class.

--- seatracker.py
+++ seatracker.py
@@ -93,3 +93,53 @@ class _Grid:
             for i in range(self.t_mask.shape[1]):
                 for j in range(self.t_mask.shape[2]):
                     self.depth[i, j] = gdepw_0[int(mbathy[i, j]), i, j]
+
+
+@attr.s
+class _ModelField:
+    """NEMO model results field.
+
+    :param model_field_file: Path and file name of the NEMO model results field
+                             file to load.
+    :type model_field_file: :py:class:`pathlib.Path` or str
+    """
+
+    model_field_file = attr.ib()
+    #: Model results field :py:class:`netCDF4.Dataset` instance.
+    dataset = attr.ib(init=False, default=None)
+    #: Model results time step [s].
+    delta_t = attr.ib(init=False, default=None)
+    ##TODO: Get Susan to describe model results field coords array
+    coords = attr.ib(init=False, default=None)
+    #: Model results field values 4d array.
+    values = attr.ib(init=False, default=None)
+
+    def setup(self, depth_var):
+        """Set up the model results field arrays.
+
+        Note that the actual loading of the field values as well as finalization
+        of the field coordinates are specific to different field variables,
+        so must be done after this method is called.
+
+        :param str depth_var: Name of the NEMO depth variable for the field.
+        """
+        self.dataset = netCDF4.Dataset(self.model_field_file)
+        times = self.dataset['time_counter'][:3]
+        self.delta_t = times[1] - times[0]
+        x_coords = range(self.dataset.dimensions['x'].size)
+        y_coords = range(self.dataset.dimensions['y'].size)
+        depthsize = self.dataset[depth_var][:].shape[0]
+        z_coords = numpy.linspace(0, depthsize, depthsize + 1) - 0.5
+        longaxis = max(len(x_coords), len(y_coords), len(z_coords), len(times))
+        self.coords = numpy.zeros((4, longaxis))
+        self.coords[Dim4d.t, 0:len(times)] = times
+        self.coords[Dim4d.t, len(times):] = max(times)
+        self.coords[Dim4d.z, 0:len(z_coords)] = z_coords
+        self.coords[Dim4d.z, len(z_coords):] = max(z_coords)
+        self.coords[Dim4d.y, 0:len(y_coords)] = y_coords
+        self.coords[Dim4d.y, len(y_coords):] = max(y_coords)
+        self.coords[Dim4d.x, 0:len(x_coords)] = x_coords
+        self.coords[Dim4d.x, len(x_coords):] = max(x_coords)
+        self.values = numpy.zeros(
+            (3, len(z_coords), len(y_coords), len(x_coords))
+        )
