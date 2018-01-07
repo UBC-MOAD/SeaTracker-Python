32f5cd20cdd02aa4b9fd94ee8015c577ffde3e9f
Doug Latornell <djl@douglatornell.ca>
2018-01-07 09:21:12 -0500

    Move transitional functions-in-module implementation aside.

--- profiling_module.py
+++ profiling_module.py
@@ -17,7 +17,7 @@
 import numpy
 from scipy.integrate import ode
 
-import seatracker
+import seatracker_module
 
 mesh_mask_path = '../../MEOPAR/grid/mesh_mask201702.nc'
 u_field_path = 'hindcast/SalishSea_1h_20160801_20160801_grid_U.nc'
@@ -27,22 +27,22 @@ tracer_fields_path = 'hindcast/SalishSea_1h_20160801_20160801_grid_T.nc'
 
 
 def main():
-    t_mask, e1u, e2v, e3w0, totaldepth = seatracker.initialize_mesh(
+    t_mask, e1u, e2v, e3w0, totaldepth = seatracker_module.initialize_mesh(
         mesh_mask_path
     )
     (
         u, v, w, tcorrs, t_coords, u_coords, v_coords, w_coords, deltat,
         nextindex, e3w, udataset, vdataset, wdataset, tdataset
-    ) = seatracker.get_initial_data(
+    ) = seatracker_module.get_initial_data(
         u_field_path, v_field_path, w_field_path, tracer_fields_path,
         totaldepth, e3w0
     )
-    tc, yi = seatracker.grid_points(t_coords, t_mask, 25 - 1, 446 - 1, 304 - 1)
+    tc, yi = seatracker_module.grid_points(t_coords, t_mask, 25 - 1, 446 - 1, 304 - 1)
     t0 = tcorrs[0]
     dt = deltat / 10
     y0 = numpy.copy(yi)
     yp = numpy.ndarray.flatten(y0)
-    integrator = ode(seatracker.derivatives).set_integrator(
+    integrator = ode(seatracker_module.derivatives).set_integrator(
         'dopri5', atol=0.01
     )
     integrator.set_initial_value(yp, t0).set_f_params(
@@ -55,7 +55,7 @@ def main():
     # more segments
     for count in range(2):
         # update arrays
-        tcorrs, u_coords, v_coords, w_coords, u, v, w, nextindex, e3w = seatracker.update_arrays(
+        tcorrs, u_coords, v_coords, w_coords, u, v, w, nextindex, e3w = seatracker_module.update_arrays(
             totaldepth, e3w0, e3w, tcorrs, u_coords, v_coords, w_coords, u, v,
             w, deltat, nextindex, udataset, vdataset, wdataset, tdataset
         )
