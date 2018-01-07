ca329249ff6271b1162e1ad6560c14f250eb9f54
Doug Latornell <djl@douglatornell.ca>
2018-01-07 09:11:37 -0500

    Update copyright years range to include 2018.

--- profiling.py
+++ profiling.py
@@ -1,5 +1,5 @@
-# Copyright 2017 The Susan Allen, Doug Latornell,
-# and The University of British Columbia
+# Copyright 2017-2018 Susan Allen, Doug Latornell, and The University of
+# British Columbia
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
